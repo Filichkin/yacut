621b3d69ffc20db86642963faf5bcd8a1c3e9e97
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-07-20 22:56:28 +0300

    created utils.py

---
+++ yacut/utils.py
@@ -0,0 +1,26 @@
+from random import choice
+import string
+
+from .constants import DEFAULT_SHORT_URL_LENGTH
+from .models import URLMap
+
+
+def generate_short_id():
+    allowed_symbols = string.ascii_letters + string.digits
+    return ''.join(
+        choice(allowed_symbols) for _ in range(DEFAULT_SHORT_URL_LENGTH)
+    )
+
+
+def get_unique_short_id():
+    short_id = generate_short_id()
+    if URLMap.query.filter_by(short=short_id).first():
+        return get_unique_short_id()
+    return short_id
+
+
+def create_url_map(original_url, custom_id=None):
+    url_map = URLMap()
+    url_map.original = original_url
+    url_map.short = custom_id if custom_id else get_unique_short_id()
+    return url_map
